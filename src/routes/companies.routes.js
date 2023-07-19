33af3853c697a14f2608ad0462bbb6c4f44d70ad
madukaife <madukaifeol@gmail.com>
2023-07-19 13:18:22 +0100

    made no changes

--- src/routes/companies.routes.js
+++ src/routes/companies.routes.js
@@ -3,8 +3,6 @@ const router = express.Router()
 const companyController = require('../controllers/companies.controllers');
 const authMiddleware = require("../middlewares/auth")
 
-
-
 router.post('/create-account', companyController.createAccount);
 router.post('/create-admin',authMiddleware.authenticate,companyController.createAdmin);
 router.post('/create-staff',authMiddleware.authenticate, companyController.createStaff);
@@ -12,6 +10,5 @@ router.post('/login', companyController.login);
 router.get('/getAllCompanies', companyController.getAllCompanies);
 router.post('/forgot-password', companyController.forgotPassword);
 router.post('/reset-password', companyController.resetPassword);
-
 
 module.exports = router;
